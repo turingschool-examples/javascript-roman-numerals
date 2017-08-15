8d3c7671296f8c9f3c912c6fb18b5301b3d89cfc
rongxanh88 <rongxanh88@gmail.com>
2017-08-15 08:36:09 -0600

    pass new roman numeral method test

--- romanize.js
+++ romanize.js
@@ -8,6 +8,17 @@ let oldValues = {
   1   : 'I'
 }
 
+let newValues = {
+  4:	 'IV',
+  9:	 'IX',
+  40:	 'XL',
+  90:	 'XC',
+  400: 'CD',
+  900: 'CM'
+}
+
+let allValues = Object.assign({}, oldValues, newValues);
+
 function oldRoman(number) {
   let romanNumerals = ''
 
@@ -15,13 +26,29 @@ function oldRoman(number) {
     Object.keys(oldValues)
           .sort((a,b) => { return b - a })
           .forEach((key) => {
-      while (number % key !== number) {
-        romanNumerals += oldValues[key]
-        number -= key
-      }
-    })
+            while (number % key !== number) {
+              romanNumerals += oldValues[key]
+              number -= key
+            }
+          })
   }
   return romanNumerals
 }
 
-var exports = module.exports = { oldRoman }
+function newRoman(number) {
+  let romanNumerals = ''
+
+  while (number > 0) {
+    Object.keys(allValues)
+          .sort((a,b) => { return b - a })
+          .forEach((key) => {
+            while (number % key !== number) {
+              romanNumerals += allValues[key]
+              number -= key
+            }
+          })
+  }
+  return romanNumerals
+}
+
+var exports = module.exports = { oldRoman, newRoman }
