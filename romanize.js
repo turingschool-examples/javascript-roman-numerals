e6f947fd1aa812e576cdce1ddd7ddb51a269b7c8
rongxanh88 <rongxanh88@gmail.com>
2017-08-15 08:08:55 -0600

    create test for old style roman numerals

---
+++ romanize.js
@@ -0,0 +1,15 @@
+let oldValues = {
+  1000: 'M',
+  500 : 'D',
+  100 : 'C',
+  50  : 'L',
+  10  : 'X',
+  5   : 'V',
+  1   : 'I'
+}
+
+function oldRoman(number) {
+  return 0
+}
+
+var exports = module.exports = { oldRoman }
---
+++ test/romanize-test.js
@@ -0,0 +1,11 @@
+const assert = require('chai').assert
+const romanizer = require('../romanize')
+
+describe('Roman Numeral Converter', () => {
+  it('converts a number to old style roman numerals', () => {
+    number = 145
+    result = romanizer.oldRoman(number)
+
+    assert.equal(result, 'CXXXXV')
+  })
+})
