d5d2bc401b401c4b001d012517f09fa26157274a
wratterman <will.ratterman@gmail.com>
2017-10-02 16:45:09 -0600

    inital setup

---
+++ test/new-roman-test.js
@@ -0,0 +1,10 @@
+const assert = require('chai').assert
+const convertToNewRoman = require('../../lib/new-roman.js')
+
+describe('convertToNewRoman functionality', function() {
+  context('convertToNewRoman function', function(){
+    it('it can convert a number to new Roman Numerals', function(){
+
+    })
+  })
+})
---
+++ test/old-roman-test.js
@@ -0,0 +1,10 @@
+const assert = require('chai').assert
+const convertToOldRoman = require('../../lib/old-roman.js')
+
+describe('convertToOldRoman functionality', function() {
+  context('convertToOldRoman function', function(){
+    it('it can convert a number to old Roman Numerals', function(){
+
+    })
+  })
+})
