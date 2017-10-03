28a2e72244482662093170085eb9e7e883b76b20
BFunk <bret.e.funk@gmail.com>
2017-10-02 20:28:42 -0600

    working test and methods but uuuuuuuugly

--- tests/app-test.js
+++ tests/app-test.js
@@ -4,5 +4,14 @@ const roman = require('../app')
 describe('convert arabic numerals to roman numerals', function() {
   it('accurately converts to roman numerals', function() {
     assert.deepEqual(roman.convert(5), 'V')
+    assert.deepEqual(roman.convert(6), 'VI')
+    assert.deepEqual(roman.convert(8), 'VIII')
+    assert.deepEqual(roman.convert(4), 'IV')
+    assert.deepEqual(roman.convert(9), 'IX')
+    assert.deepEqual(roman.convert(14), 'XIV')
+    assert.deepEqual(roman.convert(44), 'XLIV')
+    assert.deepEqual(roman.convert(99), 'XCIX')
+    assert.deepEqual(roman.convert(400), 'CD')
+    assert.deepEqual(roman.convert(944), 'CMXLIV')
   });
 });
