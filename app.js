aee22c3f8025c9edae1df2bb90d46325b4730793
BFunk <bret.e.funk@gmail.com>
2017-10-02 20:53:34 -0600

    unsucessfully added the switch statement, going back to the if else statements

--- app.js
+++ app.js
@@ -1,124 +1,49 @@
 function convert(number) {
   let roman = "";
   while (number > 0) {
-    switch (number) {
-      case number >= 1000:
-        number -= 1000;
-        roman += "M";
-        console.log(number);
-        break;
-      case number >= 900:
-        number -= 900;
-        roman += "CM";
-        console.log(number);
-        break;
-      case number >= 500:
-        number -= 500;
-        roman += "D";
-        console.log(number);
-        break;
-      case number >= 400:
-        number -= 400;
-        roman += "CD";
-        console.log(number);
-        break;
-      case number >= 100:
-        number -= 100;
-        roman += "C";
-        console.log(number);
-        break;
-      case number >= 90:
-        number -= 90;
-        roman += "XC";
-        console.log(number);
-        break;
-      case number >= 50:
-        number -= 50;
-        roman += "L";
-        console.log(number);
-        break;
-      case number >= 40:
-        number -= 40;
-        roman += "XL";
-        console.log(number);
-        break;
-      case number >= 10:
-        number -= 10;
-        roman += "X";
-        console.log(number);
-        break;
-      case number >= 9:
-        number -= 9;
-        roman += "IX";
-        console.log(number);
-        break;
-      case number >= 5:
-        number -= 5;
-        roman += "V";
-        console.log(number);
-        break;
-      case number >= 4:
-        number -= 4;
-        roman += "IV";
-        console.log(number);
-        break;
-      case number >= 1:
-        number -= 1;
-        roman += "I";
-        console.log(number);
-        break;
+    if (number >= 1000) {
+      number -= 1000;
+      roman += "M";
+    } else if (number >= 900) {
+      number -= 900;
+      roman += "CM";
+    } else if (number >= 500) {
+      number -= 500;
+      roman += "D";
+    } else if (number >= 400) {
+      number -= 400;
+      roman += "CD";
+    } else if (number >= 100) {
+      number -= 100;
+      roman += "C";
+    } else if (number >= 90) {
+      number -= 90;
+      roman += "XC";
+    } else if (number >= 50) {
+      number -= 50;
+      roman += "L";
+    } else if (number >= 40) {
+      number -= 40;
+      roman += "XL";
+    } else if (number >= 10) {
+      number -= 10;
+      roman += "X";
+    } else if (number >= 9) {
+      number -= 9;
+      roman += "IX";
+    } else if (number >= 5) {
+      number -= 5;
+      roman += "V";
+    } else if (number >= 4) {
+      number -= 4;
+      roman += "IV";
+    } else if (number >= 1) {
+      number -= 1;
+      roman += "I";
     };
   };
   return roman;
-
-  //while (number > 0) {
-    //if (number >= 1000) {
-      //number -= 1000;
-      //roman += "M";
-    //} else if (number >= 900) {
-      //number -= 900;
-      //roman += "CM";
-    //} else if (number >= 500) {
-      //number -= 500;
-      //roman += "D";
-    //} else if (number >= 400) {
-      //number -= 400;
-      //roman += "CD";
-    //} else if (number >= 100) {
-      //number -= 100;
-      //roman += "C";
-    //} else if (number >= 90) {
-      //number -= 90;
-      //roman += "XC";
-    //} else if (number >= 50) {
-      //number -= 50;
-      //roman += "L";
-    //} else if (number >= 40) {
-      //number -= 40;
-      //roman += "XL";
-    //} else if (number >= 10) {
-      //number -= 10;
-      //roman += "X";
-    //} else if (number >= 9) {
-      //number -= 9;
-      //roman += "IX";
-    //} else if (number >= 5) {
-      //number -= 5;
-      //roman += "V";
-    //} else if (number >= 4) {
-      //number -= 4;
-      //roman += "IV";
-    //} else if (number >= 1) {
-      //number -= 1;
-      //roman += "I";
-    //};
-  //};
-  //return roman;
 };
-
-
-console.log(convert(44));
-
 
 module.exports = {
   convert: convert
