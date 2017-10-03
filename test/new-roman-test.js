0df0a74d26d3936e1032a6c784c6ec25a6bf07c0
wratterman <will.ratterman@gmail.com>
2017-10-02 21:59:59 -0600

    roman numerals complete and tested

--- test/new-roman-test.js
+++ test/new-roman-test.js
@@ -1,10 +1,58 @@
 const assert = require('chai').assert
-const convertToNewRoman = require('../../lib/new-roman.js')
+const convertToNewRoman = require('../lib/new-roman.js')
 
 describe('convertToNewRoman functionality', function() {
   context('convertToNewRoman function', function(){
     it('it can convert a number to new Roman Numerals', function(){
+      let arabicNumerals = 4;
+      let answer = convertToNewRoman(arabicNumerals);
 
+      assert.equal(answer, "IV");
+
+      arabicNumerals = 9;
+      answer = convertToNewRoman(arabicNumerals);
+
+      assert.equal(answer, "IX");
+
+      arabicNumerals = 10;
+      answer = convertToNewRoman(arabicNumerals);
+
+      assert.equal(answer, "X");
+
+      arabicNumerals = 11;
+      answer = convertToNewRoman(arabicNumerals);
+
+      assert.equal(answer, "XI");
+
+      arabicNumerals = 14;
+      answer = convertToNewRoman(arabicNumerals);
+
+      assert.equal(answer, "XIV");
+
+      arabicNumerals = 44;
+      answer = convertToNewRoman(arabicNumerals);
+
+      assert.equal(answer, "XLIV");
+
+      arabicNumerals = 99;
+      answer = convertToNewRoman(arabicNumerals);
+
+      assert.equal(answer, "XCIX");
+
+      arabicNumerals = 400;
+      answer = convertToNewRoman(arabicNumerals);
+
+      assert.equal(answer, "CD");
+
+      arabicNumerals = 944;
+      answer = convertToNewRoman(arabicNumerals);
+
+      assert.equal(answer, "CMXLIV");
+
+      arabicNumerals = 0;
+      answer = convertToNewRoman(arabicNumerals);
+
+      assert.equal(answer, null);
     })
   })
 })
--- test/old-roman-test.js
+++ test/old-roman-test.js
@@ -1,10 +1,68 @@
 const assert = require('chai').assert
-const convertToOldRoman = require('../../lib/old-roman.js')
+const convertToOldRoman = require('../lib/old-roman.js')
 
 describe('convertToOldRoman functionality', function() {
   context('convertToOldRoman function', function(){
     it('it can convert a number to old Roman Numerals', function(){
+      let arabicNumerals = 1;
+      let answer = convertToOldRoman(arabicNumerals);
 
+      assert.equal(answer, "I");
+
+      arabicNumerals = 4;
+      answer = convertToOldRoman(arabicNumerals);
+
+      assert.equal(answer, "IIII");
+
+      arabicNumerals = 5;
+      answer = convertToOldRoman(arabicNumerals);
+
+      assert.equal(answer, "V");
+
+      arabicNumerals = 10;
+      answer = convertToOldRoman(arabicNumerals);
+
+      assert.equal(answer, "X");
+
+      arabicNumerals = 14;
+      answer = convertToOldRoman(arabicNumerals);
+
+      assert.equal(answer, "XIIII");
+
+      arabicNumerals = 15;
+      answer = convertToOldRoman(arabicNumerals);
+
+      assert.equal(answer, "XV");
+
+      arabicNumerals = 50;
+      answer = convertToOldRoman(arabicNumerals);
+
+      assert.equal(answer, "L");
+
+      arabicNumerals = 100;
+      answer = convertToOldRoman(arabicNumerals);
+
+      assert.equal(answer, "C");
+
+      arabicNumerals = 500;
+      answer = convertToOldRoman(arabicNumerals);
+
+      assert.equal(answer, "D");
+
+      arabicNumerals = 1000;
+      answer = convertToOldRoman(arabicNumerals);
+
+      assert.equal(answer, "M");
+
+      arabicNumerals = 3000;
+      answer = convertToOldRoman(arabicNumerals);
+
+      assert.equal(answer, "MMM");
+
+      arabicNumerals = 0;
+      answer = convertToOldRoman(arabicNumerals);
+
+      assert.equal(answer, null);
     })
   })
 })
