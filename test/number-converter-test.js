e1c2fe87828d969c902266983b1b6ef2f4057786
Brandon Stewart <boveus@gmail.com>
2017-11-27 21:00:12 -0700

    final solution

--- test/number-converter-test.js
+++ test/number-converter-test.js
@@ -1,18 +1,18 @@
 var assert = require('assert');
 var expect    = require("chai").expect;
-var triangleFunctions = require("../lib/number-converter");
+var converter = require("../lib/number-converter");
 describe('number-converter', function() {
   describe('convertToOldRoman', function() {
     it('converts arabic numbers to roman numerals', function() {
-      var one = triangleFunctions.isTriangle(1)
-      var six = triangleFunctions.isTriangle(6)
-      var twelve = triangleFunctions.isTriangle(12)
-      var sixty = triangleFunctions.isTriangle(60)
-      var oneHundredTen = triangleFunctions.isTriangle(110)
-      var fiveHundredOne = triangleFunctions.isTriangle(501)
-      var oneThousandThree = triangleFunctions.isTriangle(1003)
+      var one = converter.convertToOldRoman(1)
+      var six = converter.convertToOldRoman(6)
+      var twelve = converter.convertToOldRoman(12)
+      var sixty = converter.convertToOldRoman(60)
+      var oneHundredTen = converter.convertToOldRoman(110)
+      var fiveHundredOne = converter.convertToOldRoman(501)
+      var oneThousandThree = converter.convertToOldRoman(1003)
 
-      assert.deepEqual(one, "I")
+      assert.deepEqual(one, "I", "Converts one")
       assert.deepEqual(six, "VI")
       assert.deepEqual(twelve, "XII")
       assert.deepEqual(sixty, "LX")
