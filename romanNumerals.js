b5af09b1fa816759025f6bb4f31992875b0a18d8
Becki Srofe <blf_23@yahoo.com>
2017-11-27 20:57:35 -0700

    Refactor old roman

--- romanNumerals.js
+++ romanNumerals.js
@@ -1,47 +1,22 @@
 function convertToOldRoman(num) {
   var newNum = num
   var roman = ""
-  var thousand = Math.floor(num/1000)
-  if (thousand !== 0){
-    newNum = newNum - (thousand * 1000)
-    var string = "M"
-    roman += string.repeat(thousand)
-  }
-  var fiveHundreds = Math.floor(newNum/500)
-  if (fiveHundreds !== 0) {
-    newNum = newNum - ( fiveHundreds * 500)
-    var string = "D"
-    roman += string.repeat(fiveHundreds)
-  }
-  var oneHundreds = Math.floor(newNum/100)
-  if (oneHundreds !== 0) {
-    newNum = newNum - ( oneHundreds * 100)
-    var string = "C"
-    roman += string.repeat(oneHundreds)
-  }
-  var fifty = Math.floor(newNum/50)
-  if (fifty !== 0) {
-    newNum = newNum - ( fifty * 50)
-    var string = "L"
-    roman += string.repeat(fifty)
-  }
-  var ten = Math.floor(newNum/10)
-  if (ten !== 0) {
-    newNum = newNum - ( ten * 10)
-    var string = "X"
-    roman += string.repeat(ten)
-  }
-  var five = Math.floor(newNum/5)
-  if (five !== 0) {
-    newNum = newNum - (five * 5)
-    var string = "V"
-    roman += string.repeat(five)
-  }
-  var one = Math.floor(newNum)
-  if (five !== 0) {
-    var string = "I"
-    roman += string.repeat(one)
-  }
+  var array = [{number: 1000, symbol: "M"},
+              {number: 500, symbol: "D"},
+              {number: 100, symbol: "C"},
+              {number: 50, symbol: "L"},
+              {number: 10, symbol: "X"},
+              {number: 5, symbol: "V"},
+              {number: 1, symbol: "I"}]
+
+  array.forEach(function (placeValue) {
+    var temp = Math.floor(newNum/placeValue.number)
+    if (temp !== 0) {
+      newNum = newNum - (temp * placeValue.number)
+      var string = placeValue.symbol
+      roman += string.repeat(temp)
+    }
+  })
   return roman
 }
 
