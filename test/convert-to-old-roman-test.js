e1a4db3e1c0657e4c35812861f0ca48e785ef9b6
Ricky Amparo <rickyamparo@gmail.com>
2017-11-27 19:54:36 -0700

    finish old numeral function and test

---
+++ test/convert-to-old-roman-test.js
@@ -0,0 +1,8 @@
+const assert = require('chai').assert;
+const convertToOldRoman = require('../convert-to-old-roman')
+
+describe('convert to old roman', function(){
+  it('can convert a number to old roman numeral', function(){
+    assert.equal(convertToOldRoman(4), 'IIII')
+  })
+})
