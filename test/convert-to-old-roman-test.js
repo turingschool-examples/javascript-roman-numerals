d3aabc1cdaacd4578c514d2cff94f91760c6e116
Ricky Amparo <rickyamparo@gmail.com>
2017-11-27 19:55:59 -0700

    add sad path testing to the convert to old roman

--- test/convert-to-old-roman-test.js
+++ test/convert-to-old-roman-test.js
@@ -4,5 +4,7 @@ const convertToOldRoman = require('../convert-to-old-roman')
 describe('convert to old roman', function(){
   it('can convert a number to old roman numeral', function(){
     assert.equal(convertToOldRoman(4), 'IIII')
+    assert.equal(convertToOldRoman(2001), 'MMI')
+    assert.equal(convertToOldRoman(8213), 'MMMMMMMMCCXIII')
   })
 })
