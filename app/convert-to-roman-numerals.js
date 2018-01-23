2fe5668d4ada9cc9717811abc2fdeaae843c146f
sidewinder2020 <sdwinder1@gmail.com>
2018-01-22 18:45:43 -0700

    completed new roman numeral conversion - ALSO ugly - need to refactor - too redundant

--- app/convert-to-roman-numerals.js
+++ app/convert-to-roman-numerals.js
@@ -1,69 +1,198 @@
 exports.convertToOldRoman = function(num) {
-  var numArray = []
+  if (num <= 0) {
+    return "cannot convert a number less than or equal to zero";
+  }
+  else {
+    var numArray = []
 
-  var convertToNumArray = function(num) {
-    if (num / 1000 >= 1) {
-      numArray.push(1000);
-      newNum = num - 1000;
-      convertToNumArray(newNum);
-    }
-    else if (num / 500 >= 1) {
-      numArray.push(500);
-      newNum = num - 500;
-      convertToNumArray(newNum);
-    }
-    else if (num / 100 >= 1) {
-      numArray.push(100);
-      newNum = num - 100;
-      convertToNumArray(newNum);
-    }
-    else if (num / 50 >= 1) {
-      numArray.push(50);
-      newNum = num - 50;
-      convertToNumArray(newNum);
-    }
-    else if (num / 10 >= 1) {
-      numArray.push(10);
-      newNum = num - 10;
-      convertToNumArray(newNum);
-    }
-    else if (num / 5 >= 1) {
-      numArray.push(5);
-      newNum = num - 5;
-      convertToNumArray(newNum);
-    }
-    else if (num / 1 >= 1) {
-      numArray.push(1);
-      newNum = num - 1;
-      convertToNumArray(newNum);
-    }
-    return numArray;
-  };
-  convertToNumArray(num);
+    var convertToNumArray = function(num) {
+      if (num / 1000 >= 1) {
+        numArray.push(1000);
+        newNum = num - 1000;
+        convertToNumArray(newNum);
+      }
+      else if (num / 500 >= 1) {
+        numArray.push(500);
+        newNum = num - 500;
+        convertToNumArray(newNum);
+      }
+      else if (num / 100 >= 1) {
+        numArray.push(100);
+        newNum = num - 100;
+        convertToNumArray(newNum);
+      }
+      else if (num / 50 >= 1) {
+        numArray.push(50);
+        newNum = num - 50;
+        convertToNumArray(newNum);
+      }
+      else if (num / 10 >= 1) {
+        numArray.push(10);
+        newNum = num - 10;
+        convertToNumArray(newNum);
+      }
+      else if (num / 5 >= 1) {
+        numArray.push(5);
+        newNum = num - 5;
+        convertToNumArray(newNum);
+      }
+      else if (num / 1 >= 1) {
+        numArray.push(1);
+        newNum = num - 1;
+        convertToNumArray(newNum);
+      }
+      return numArray;
+    };
+    convertToNumArray(num);
 
-  var finalNum = numArray.reduce(function(final, num) {
-    if (num === 1000) {
-      return final + "M";
-    }
-    else if (num === 500) {
-      return final + "D";
-    }
-    else if (num === 100) {
-      return final + "C";
-    }
-    else if (num === 50) {
-      return final + "L";
-    }
-    else if (num === 10) {
-      return final + "X";
-    }
-    else if (num === 5) {
-      return final + "V";
-    }
-    else {
-      return final + "I"
-    }
-  }, "");
-  
-  console.log(finalNum);
+    var finalNum = numArray.reduce(function(final, num) {
+      if (num === 1000) {
+        return final + "M";
+      }
+      else if (num === 500) {
+        return final + "D";
+      }
+      else if (num === 100) {
+        return final + "C";
+      }
+      else if (num === 50) {
+        return final + "L";
+      }
+      else if (num === 10) {
+        return final + "X";
+      }
+      else if (num === 5) {
+        return final + "V";
+      }
+      else {
+        return final + "I"
+      }
+    }, "");
+
+    return finalNum;
+  }
+}
+
+
+exports.convertToNewRoman = function(num) {
+  if (num <= 0) {
+    return "cannot convert a number less than or equal to zero";
+  }
+  else {
+    var numArray = []
+
+    var convertToNumArray = function(num) {
+      if (num / 1000 >= 1) {
+        numArray.push(1000);
+        newNum = num - 1000;
+        convertToNumArray(newNum);
+      }
+      else if (num / 900 >= 1) {
+        numArray.push(900);
+        newNum = num - 900;
+        convertToNumArray(newNum);
+      }
+      else if (num / 500 >= 1) {
+        numArray.push(500);
+        newNum = num - 500;
+        convertToNumArray(newNum);
+      }
+      else if (num / 400 >= 1) {
+        numArray.push(400);
+        newNum = num - 400;
+        convertToNumArray(newNum);
+      }
+      else if (num / 100 >= 1) {
+        numArray.push(100);
+        newNum = num - 100;
+        convertToNumArray(newNum);
+      }
+      else if (num / 90 >= 1) {
+        numArray.push(90);
+        newNum = num - 90;
+        convertToNumArray(newNum);
+      }
+      else if (num / 50 >= 1) {
+        numArray.push(50);
+        newNum = num - 50;
+        convertToNumArray(newNum);
+      }
+      else if (num / 40 >= 1) {
+        numArray.push(40);
+        newNum = num - 40;
+        convertToNumArray(newNum);
+      }
+      else if (num / 10 >= 1) {
+        numArray.push(10);
+        newNum = num - 10;
+        convertToNumArray(newNum);
+      }
+      else if (num / 9 >= 1) {
+        numArray.push(9);
+        newNum = num - 9;
+        convertToNumArray(newNum);
+      }
+      else if (num / 5 >= 1) {
+        numArray.push(5);
+        newNum = num - 5;
+        convertToNumArray(newNum);
+      }
+      else if (num / 4 >= 1) {
+        numArray.push(4);
+        newNum = num - 4;
+        convertToNumArray(newNum);
+      }
+      else if (num / 1 >= 1) {
+        numArray.push(1);
+        newNum = num - 1;
+        convertToNumArray(newNum);
+      }
+      return numArray;
+    };
+    convertToNumArray(num);
+
+    var finalNum = numArray.reduce(function(final, num) {
+      if (num === 1000) {
+        return final + "M";
+      }
+      else if (num === 900) {
+        return final + "CM"
+      }
+      else if (num === 500) {
+        return final + "D";
+      }
+      else if (num === 400) {
+        return final + "CD"
+      }
+      else if (num === 100) {
+        return final + "C";
+      }
+      else if (num === 90) {
+        return final + "XC"
+      }
+      else if (num === 50) {
+        return final + "L";
+      }
+      else if (num === 40) {
+        return final + "XL";
+      }
+      else if (num === 10) {
+        return final + "X";
+      }
+      else if (num === 9) {
+        return final + "IX"
+      }
+      else if (num === 5) {
+        return final + "V";
+      }
+      else if (num === 4) {
+        return final + "IV";
+      }
+      else {
+        return final + "I"
+      }
+    }, "");
+
+    return finalNum;
+  }
 }
--- test/convert-to-roman-numerals-test.js
+++ test/convert-to-roman-numerals-test.js
@@ -5,7 +5,27 @@ describe("Convert to Roman Numerals functions", function() {
   it("converts integers to either old or new roman numerals", function() {
 
     var oldRoman1 = convert.convertToOldRoman(4);
+    var oldRoman2 = convert.convertToOldRoman(4356);
+    var oldRoman3 = convert.convertToOldRoman(2347);
+    var oldRoman4 = convert.convertToOldRoman(555);
+    var oldRoman5 = convert.convertToOldRoman(0);
 
     expect(oldRoman1).to.deep.equal("IIII");
+    expect(oldRoman2).to.deep.equal("MMMMCCCLVI");
+    expect(oldRoman3).to.deep.equal("MMCCCXXXXVII");
+    expect(oldRoman4).to.deep.equal("DLV");
+    expect(oldRoman5).to.deep.equal("cannot convert a number less than or equal to zero");
+
+    var newRoman1 = convert.convertToNewRoman(4);
+    var newRoman2 = convert.convertToNewRoman(3356);
+    var newRoman3 = convert.convertToNewRoman(2947);
+    var newRoman4 = convert.convertToNewRoman(595);
+    var newRoman5 = convert.convertToNewRoman(0);
+
+    expect(newRoman1).to.deep.equal("IV");
+    expect(newRoman2).to.deep.equal("MMMCCCLVI");
+    expect(newRoman3).to.deep.equal("MMCMXLVII");
+    expect(newRoman4).to.deep.equal("DXCV");
+    expect(newRoman5).to.deep.equal("cannot convert a number less than or equal to zero");
   })
 })
