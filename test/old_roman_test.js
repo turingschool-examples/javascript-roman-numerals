4e73899cb30dad7b0d69e8e04957963e20b0651a
abarnes26 <abarnes26@yahoo.com>
2018-03-12 22:36:56 -0600

    completed rafactor of old roman

--- test/old_roman_test.js
+++ test/old_roman_test.js
@@ -3,7 +3,7 @@ const convertToOldRoman = require('../lib/old_roman.js')
 
 describe('Old Roman Converter', function() {
   it('can convert Arabic to Old Roman', function() {
-    // assert.equal(convertToOldRoman(4), 'IIII')
+    assert.equal(convertToOldRoman(4), 'IIII')
     assert.equal(convertToOldRoman(13), 'XIII')
     assert.equal(convertToOldRoman(61), 'LXI')
     assert.equal(convertToOldRoman(115),'CXV')
