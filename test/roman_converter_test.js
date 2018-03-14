4e009fa16420f0e5490d78294ebf1db51859b3a5
ZacharyLandes <zach.g.landes@gmail.com>
2018-03-13 21:08:05 -0600

    first commit

--- test/roman_converter_test.js
+++ test/roman_converter_test.js
@@ -1,17 +1,19 @@
 const assert = require('chai').assert
-const [convertToOldRoman, convertToNewRoman] = require('../lib/roman_converter')
+const [convertToOldRoman, convertToModernRoman] = require('../lib/roman_converter')
 
 describe("it can convert to roman", () => {
+  
   it("converts to old roman", () => {
-    assert.strictEqual(convertToOldRoman(4), 'IIII')
-    assert.strictEqual(convertToOldRoman(10), 'X')
-    assert.strictEqual(convertToOldRoman(3000), 'MMM')
-    assert.strictEqual(convertToOldRoman(245), 'CCXXXXV')
+    assert.equal(convertToOldRoman(22), 'XXII')
+    assert.equal(convertToOldRoman(105), 'CV')
+    assert.equal(convertToOldRoman(545), 'DXXXXV')
+    assert.equal(convertToOldRoman(1200), 'MCC')
   })
-  it("converts to new roman", () => {
-    assert.strictEqual(convertToNewRoman(4), 'IV')
-    assert.strictEqual(convertToNewRoman(10), 'X')
-    assert.strictEqual(convertToNewRoman(3000), 'MMM')
-    assert.strictEqual(convertToNewRoman(245), 'CCXLV')
+
+  it("converts to modern roman", () => {
+    assert.equal(convertToModernRoman(2), 'II')
+    assert.equal(convertToModernRoman(105), 'CV')
+    assert.equal(convertToModernRoman(2234), 'MMCCXXXIV')
+    assert.equal(convertToModernRoman(543), 'DXLIII')
   })
 })
